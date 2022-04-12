50c833840645338515baf87e4637b39fcf0a244a
laixingyu <laixingyu_a@163.com>
2022-04-12 18:04:47 +0800

    2022.4.12

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,74 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <main>
+    <div class="container">
+      <h1>欢迎使用待办事项</h1>
+      <todo-add :tid="todos.length" @addTodo="addTodo"></todo-add>
+      <todo-filter
+        :selected="filter"
+        @changeFilter="filter = $event"
+      ></todo-filter>
+      <todo-list :todos="filteredTodos"></todo-list>
+    </div>
+  </main>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TodoAdd from "./components/TodoAdd";
+import TodoFilter from "./components/TodoFilter";
+import TodoList from "./components/TodoList";
+import useTodos from "@/composables/useTodos";
+import useFilteredTodos from "@/composables/useFilteredTodos";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: { TodoAdd, TodoFilter, TodoList },
+  setup() {
+    const { todos, addTodo } = useTodos();
+    const { filter, filteredTodos } = useFilteredTodos(todos);
+
+    return {
+      todos,
+      addTodo,
+      filter,
+      filteredTodos,
+    };
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+  font-family: Helvetica, "PingFang SC", "Microsoft Yahei", sans-serif;
+}
+
+/* 整个页面 */
+main {
+  width: 100vw;
+  min-height: 100vh;
+  display: grid;
+  align-items: center;
+  justify-items: center;
+  background: rgb(203, 210, 240);
+  padding: 50px 0;
+}
+
+.container {
+  width: 60%;
+  max-width: 400px;
+  box-shadow: 0 0 24px rgba(0, 0, 0, 0.15);
+  border-radius: 24px;
+  padding: 48px 28px;
+  background-color: rgb(245, 246, 252);
+}
+
+/* 标题 */
+h1 {
+  margin: 24px 0;
+  font-size: 28px;
+  color: #414873;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="todo-list">
+    <todo-item
+      v-for="todo in todos"
+      :key="todo.id"
+      :todo="todo"
+      @changeState="todo.completed = $event.target.checked"
+    ></todo-item>
+  </div>
+</template>
+
+<script>
+import TodoItem from "./TodoItem";
+
+export default {
+  name: "TodoList",
+  components: { TodoItem },
+  props: ["todos"],
+};
+</script>
+
+<style>
+.todo-list {
+  display: grid;
+  row-gap: 14px;
+}
+</style>
